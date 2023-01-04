b38d6452ca3e794558d00e4329cb7e7d10224424
Mambisi Zempare <lilbizi96@gmail.com>
2023-01-04 05:14:03 +0000

    make it a bit safe with memory lifetimes

--- randomx/src/dataset.rs
+++ randomx/src/dataset.rs
@@ -5,27 +5,30 @@ use crate::bindings::{
 };
 use crate::error::Error;
 use crate::flag::RandomxFlags;
-use std::ffi::{c_ulong};
+use std::ffi::c_ulong;
 use std::sync::Arc;
+use parking_lot::Mutex;
 
-pub struct RandomxDataset {
-    pub(crate) dataset: *mut randomx_dataset,
+pub struct RandomxDataset<'a> {
+    pub(crate) dataset: &'a mut randomx_dataset,
 }
 
-impl RandomxDataset {
+impl<'a> RandomxDataset<'a> {
     pub fn new(flags: RandomxFlags, key: &[u8]) -> Result<Self, Error> {
         unsafe {
             let cache = RandomxCache::new(flags, key)?;
             let mut dataset = RandomxDataset {
-                dataset: randomx_alloc_dataset(flags.bits()),
+                dataset: randomx_alloc_dataset(flags.bits())
+                    .as_mut()
+                    .ok_or(Error::DatasetAllocError)?,
             };
             let count = randomx_dataset_item_count();
             let num_cpus = num_cpus::get() as c_ulong;
             if num_cpus <= 1 {
                 randomx_init_dataset(dataset.dataset, cache.cache, 0, count);
             } else {
-                let arc_cache = Arc::new(cache);
-                let arc_dataset = Arc::new(dataset);
+                let arc_cache = Arc::new(Mutex::new(cache));
+                let arc_dataset = Arc::new(Mutex::new(dataset));
 
                 let size = count / num_cpus;
                 let last = count % num_cpus;
@@ -40,7 +43,12 @@ impl RandomxDataset {
                     }
                     let start_index = start;
                     handles.push(std::thread::spawn(move || {
-                        randomx_init_dataset(dataset.dataset, cache.cache, start_index, temp_size);
+                        randomx_init_dataset(
+                            (*(dataset.data_ptr())).dataset,
+                            (*(cache.data_ptr())).cache,
+                            start_index,
+                            temp_size,
+                        );
                     }));
                     start += temp_size;
                 }
@@ -50,7 +58,7 @@ impl RandomxDataset {
                 }
 
                 dataset = match Arc::try_unwrap(arc_dataset) {
-                    Ok(dataset) => dataset,
+                    Ok(dataset) => dataset.into_inner(),
                     Err(_) => return Err(Error::DatasetAllocError),
                 };
             }
@@ -60,27 +68,27 @@ impl RandomxDataset {
     }
 }
 
-unsafe impl Send for RandomxDataset {}
+unsafe impl<'a> Send for RandomxDataset<'a> {}
 
-unsafe impl Sync for RandomxDataset {}
+unsafe impl<'a> Sync for RandomxDataset<'a> {}
 
-impl Drop for RandomxDataset {
+impl<'a> Drop for RandomxDataset<'a> {
     fn drop(&mut self) {
         unsafe { randomx_release_dataset(self.dataset) }
     }
 }
 
-pub struct RandomxCache {
-    pub(crate) cache: *mut randomx_cache,
+pub struct RandomxCache<'a> {
+    pub(crate) cache: &'a mut randomx_cache,
 }
 
-impl RandomxCache {
+impl<'a> RandomxCache<'a> {
     pub fn new(flags: RandomxFlags, key: &[u8]) -> Result<Self, Error> {
-        let cache = unsafe { randomx_alloc_cache(flags.bits()) };
-
-        if cache.is_null() {
-            return Err(Error::CacheAllocError);
-        }
+        let cache = unsafe {
+            randomx_alloc_cache(flags.bits())
+                .as_mut()
+                .ok_or(Error::CacheAllocError)?
+        };
 
         unsafe {
             randomx_init_cache(cache, key.as_ptr() as *const std::ffi::c_void, key.len());
@@ -90,12 +98,12 @@ impl RandomxCache {
     }
 }
 
-impl Drop for RandomxCache {
+impl<'a> Drop for RandomxCache<'a> {
     fn drop(&mut self) {
         unsafe { randomx_release_cache(self.cache) }
     }
 }
 
-unsafe impl Send for RandomxCache {}
+unsafe impl<'a> Send for RandomxCache<'a> {}
 
-unsafe impl Sync for RandomxCache {}
+unsafe impl<'a> Sync for RandomxCache<'a> {}
--- randomx/src/lib.rs
+++ randomx/src/lib.rs
@@ -12,34 +12,48 @@ pub mod flag;
 
 pub const RANDOMX_HASH_SIZE: usize = bindings::RANDOMX_HASH_SIZE as usize;
 
-pub struct RandomX {
-    vm: *mut bindings::randomx_vm,
+pub struct RandomX<'a> {
+    vm: &'a mut bindings::randomx_vm,
 }
 
-impl RandomX {
-    pub fn new(flags: RandomxFlags, cache: &RandomxCache) -> Result<Self, Error> {
+impl<'a> RandomX<'a> {
+    pub fn new(flags: RandomxFlags, cache: &mut RandomxCache<'a>) -> Result<Self, Error> {
         let vm = unsafe {
             let flags = flags.bits();
-            let vm = bindings::randomx_create_vm(flags, cache.cache, std::ptr::null_mut());
+            let vm = bindings::randomx_create_vm(flags, cache.cache, std::ptr::null_mut()).as_mut();
             vm
         };
-        if vm.is_null() {
+        let Some(vm) = vm else {
             return Err(Error::FailedToInitializeVM);
-        }
+        };
         Ok(Self { vm })
     }
 
-    pub fn new_fast(flags: RandomxFlags, dataset: &RandomxDataset) -> Result<Self, Error> {
+    pub fn new_with_key(flags: RandomxFlags, key: &[u8]) -> Result<Self, Error> {
+        let cache = RandomxCache::new(flags, key)?;
         let vm = unsafe {
             let flags = flags.bits();
-            let vm = bindings::randomx_create_vm(flags, std::ptr::null_mut(), dataset.dataset);
+            let vm = bindings::randomx_create_vm(flags, cache.cache, std::ptr::null_mut()).as_mut();
             vm
         };
-        if vm.is_null() {
+        let Some(vm) = vm else {
             return Err(Error::FailedToInitializeVM);
-        }
+        };
         Ok(Self { vm })
     }
+
+    pub fn new_fast(flags: RandomxFlags, dataset: &mut  RandomxDataset<'a>) -> Result<Self, Error> {
+        let vm = unsafe {
+            let flags = flags.bits();
+            let vm = bindings::randomx_create_vm(flags, std::ptr::null_mut(), dataset.dataset).as_mut();
+            vm
+        };
+        let Some(vm) = vm else {
+            return Err(Error::FailedToInitializeVM);
+        };
+        Ok(Self { vm })
+    }
+
 
     pub fn calculate_hash<I: AsRef<[u8]>>(
         &mut self,
@@ -71,7 +85,7 @@ impl RandomX {
     }
 }
 
-impl Drop for RandomX {
+impl<'a> Drop for RandomX<'a> {
     fn drop(&mut self) {
         unsafe {
             bindings::randomx_destroy_vm(self.vm);
@@ -90,15 +104,18 @@ mod tests {
         let input = b"RandomX example input\0";
         let mut out = [0_u8; RANDOMX_HASH_SIZE];
 
-        let cache = RandomxCache::new(flags, key).unwrap();
+        let mut cache = RandomxCache::new(flags, key).unwrap();
 
-        let mut rx = RandomX::new(flags, &cache).unwrap();
+        let mut rx = RandomX::new(flags, &mut cache).unwrap();
         rx.calculate_hash(input, &mut out).unwrap();
         let expected = [
             138, 72, 229, 249, 219, 69, 171, 121, 217, 8, 5, 116, 196, 216, 25, 84, 254, 106, 198,
             56, 66, 33, 74, 255, 115, 194, 68, 178, 99, 48, 183, 201,
         ];
+
         assert_eq!(expected, out);
+
+        println!("{}", hex::encode(out));
     }
 
     #[test]
@@ -108,9 +125,9 @@ mod tests {
         let input = b"RandomX example input\0";
         let mut out = [0_u8; RANDOMX_HASH_SIZE];
 
-        let dataset = RandomxDataset::new(flags, key).unwrap();
+        let mut dataset = RandomxDataset::new(flags, key).unwrap();
 
-        let mut rx = RandomX::new_fast(flags, &dataset).unwrap();
+        let mut rx = RandomX::new_fast(flags, &mut dataset).unwrap();
         rx.calculate_hash(input, &mut out).unwrap();
         let expected = [
             138, 72, 229, 249, 219, 69, 171, 121, 217, 8, 5, 116, 196, 216, 25, 84, 254, 106, 198,
