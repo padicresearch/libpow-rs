b83643ae3f35a3d20169f54ca492258d6b2579cb
Mambisi Zempare <lilbizi96@gmail.com>
2023-01-08 09:00:54 +0000

    refactoring

--- randomx/src/dataset.rs
+++ randomx/src/dataset.rs
@@ -19,56 +19,58 @@ pub struct RandomxDataset {
 
 impl RandomxDataset {
     pub fn new(flags: RandomxFlags, key: &[u8]) -> Result<Self, Error> {
-        unsafe {
-            let cache = RandomxCache::new(flags, key)?;
-            let mut dataset = RandomxDataset {
-                dataset: randomx_alloc_dataset(flags.bits())
+        let cache = RandomxCache::new(flags, key)?;
+        let mut dataset = RandomxDataset {
+            dataset: unsafe {
+                randomx_alloc_dataset(flags.bits())
                     .as_mut()
-                    .ok_or(Error::DatasetAllocError)?,
-            };
-            let count = randomx_dataset_item_count();
-            let num_cpus = num_cpus::get() as c_ulong;
-            if num_cpus <= 1 {
+                    .ok_or(Error::DatasetAllocError)?
+            },
+        };
+        let count = unsafe { randomx_dataset_item_count() };
+        let num_cpus = num_cpus::get() as c_ulong;
+        if num_cpus <= 1 {
+            unsafe {
                 randomx_init_dataset(dataset.dataset, cache.cache, 0, count);
-            } else {
-                let arc_cache = Arc::new(Mutex::new(cache));
-                let arc_dataset = Arc::new(Mutex::new(dataset));
+            }
+        } else {
+            let arc_cache = Arc::new(Mutex::new(cache));
+            let arc_dataset = Arc::new(Mutex::new(dataset));
 
-                let size = count / num_cpus;
-                let last = count % num_cpus;
-                let mut start = 0;
-                let mut handles = Vec::new();
-                for i in 0..num_cpus {
-                    let cache = arc_cache.clone();
-                    let dataset = arc_dataset.clone();
-                    let mut temp_size = size;
-                    if i == num_cpus - 1 {
-                        temp_size += last;
-                    }
-                    let start_index = start;
-                    handles.push(std::thread::spawn(move || {
-                        randomx_init_dataset(
-                            (*(dataset.data_ptr())).dataset,
-                            (*(cache.data_ptr())).cache,
-                            start_index,
-                            temp_size,
-                        );
-                    }));
-                    start += temp_size;
+            let size = count / num_cpus;
+            let last = count % num_cpus;
+            let mut start = 0;
+            let mut handles = Vec::new();
+            for i in 0..num_cpus {
+                let cache = arc_cache.clone();
+                let dataset = arc_dataset.clone();
+                let mut temp_size = size;
+                if i == num_cpus - 1 {
+                    temp_size += last;
                 }
-
-                for handle in handles {
-                    handle.join().map_err(|_| Error::ThreadError)?;
-                }
-
-                dataset = match Arc::try_unwrap(arc_dataset) {
-                    Ok(dataset) => dataset.into_inner(),
-                    Err(_) => return Err(Error::DatasetAllocError),
-                };
+                let start_index = start;
+                handles.push(std::thread::spawn(move || unsafe {
+                    randomx_init_dataset(
+                        (*(dataset.data_ptr())).dataset,
+                        (*(cache.data_ptr())).cache,
+                        start_index,
+                        temp_size,
+                    );
+                }));
+                start += temp_size;
             }
 
-            Ok(dataset)
+            for handle in handles {
+                handle.join().map_err(|_| Error::ThreadError)?;
+            }
+
+            dataset = match Arc::try_unwrap(arc_dataset) {
+                Ok(dataset) => dataset.into_inner(),
+                Err(_) => return Err(Error::DatasetAllocError),
+            };
         }
+
+        Ok(dataset)
     }
 
     pub fn as_slice(&mut self) -> &[u8] {
@@ -83,21 +85,22 @@ impl RandomxDataset {
         }
     }
 
-    pub fn from_file<P: AsRef<Path>>(flags: RandomxFlags, path: P) -> Result<Self, Error> {
+    pub fn open<P: AsRef<Path>>(flags: RandomxFlags, path: P) -> Result<Self, Error> {
+        let file = OpenOptions::new().read(true).open(path)?;
+        let mmap_file = unsafe { Mmap::map(&file)? };
+        let dataset = unsafe { randomx_alloc_dataset(flags.bits()) };
         unsafe {
-            let file = OpenOptions::new().read(true).open(path)?;
-            let mmap_file = Mmap::map(&file)?;
-            let dataset = randomx_alloc_dataset(flags.bits());
             libc::memcpy(
                 randomx_get_dataset_memory(ptr),
                 mmap_file.as_ptr() as *const _,
                 randomx_dataset_item_count() as usize * RANDOMX_DATASET_ITEM_SIZE as usize,
             );
-            if dataset.is_null() {
-                return Err(Error::DatasetAllocError);
-            }
-            Ok(Self { dataset })
         }
+
+        if dataset.is_null() {
+            return Err(Error::DatasetAllocError);
+        }
+        Ok(Self { dataset })
     }
 }
 
